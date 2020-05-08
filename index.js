91c7a7424778ab06b41deb414d805e6ccc203c95
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-08 10:18:13 -0700

    mvp

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ server.use('/api/actions', actionRouter)
 server.listen(port, () => {
   console.log(`\n ❤ ❤ ❤	 LIVE  ON  ${port} ❤ ❤ ❤	`)
 })
+
+/* NOTES for weekend:
+1. Try to do stretch, would be fun
+2. Try to deploy to heroku
+3. Practice with .env
+4. Learn more about Helmet
+5. Research other 3rd party middleware
+6. Get better at thinking about edge cases
+*/
